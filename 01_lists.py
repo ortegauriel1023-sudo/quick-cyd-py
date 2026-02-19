e39f20a05638c7ab4ecc3b26427225c9b96b63b1
ortegauriel1023-sudo <ortegauriel1023@gmail.com>
2026-02-19 00:15:51 +0000

    solutions.1

--- 01_lists.py
+++ 01_lists.py
@@ -2,7 +2,11 @@
 #
 # a. Replace "None" on line 2 with a list that contains the names of the seven dwarves
 # Grumpy, Happy, Sleepy, Sneezy, Dopey, Bashful, and Doc
-# dwarves = None
+'''dwarves = ['Grumpy','Happy','Sleepy','Sneezy','Dopey','Bashful','and Doc']
+print(dwarves[0])
+
+dwarves.append('zack')
+print(dwarves)
 # print("The seven dwarves: " + str(dwarves))
 
 # b. Join the crew! Write one line below that adds your name 
@@ -13,28 +17,39 @@
 # print(dwarves[8])
 
 # d. Uncomment the line below and print Happy's name using his index in the list
-# print(dwarves[8])
+print(dwarves[1])
 
 # e. Maybe eight is two many, write a line to remove a specific dwarf's name
 # (this can be you or another dwarf)
-
+dwarves.remove('Sneezy')
+print(dwarves)
 # f. Write one line of code below so that when the list is printed again it is
 #    in alphabetical order
-
-
+dwarves.sort()
+print(dwarves)
+'''
 # print("Seven alphabetical dwarves: " + str(dwarves))
-
+'''
 # Part 2 - counting
 #
 # a. create a variable named one_to_ten that contains the numbers 0 to 100
 #    use the range function to do it without typing all the number yourself
-
+one_to_ten = list(range(0, 101))
+print(one_to_ten)
+#for one_to_ten in range(1,101): 
+    #print(one_to_ten)
 # b. use the range function a variable named evens that contains even numbers
 #    from 0 to 100 
 #    hint: the range function's third parameter will help with this
 #    https://www.w3schools.com/python/ref_func_range.asp
-
-# c. use range to create a list that counts *backward* from one to 100
-
+evens = list(range(0, 101, 2))
+print(evens)
+#for evens in range(0,101,2):
+ #   print(evens)
+# c. use range to create a list that counts *backward* from one to 100'''
+'''for backward in range (100,0,-1):
+    print(backward)
 # d. use range to create a list that counts *backward by 5* fromn 100
 #    as in 100, 95, 90, 85, 80...
+for backward in range (100,-5,-5):
+    print(backward) '''
